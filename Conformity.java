455893e7a59e51d2c345912dff8f4a43ad8695cc
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-02-11 16:31:39 +0530

    Update Conformity.java

--- Conformity.java
+++ Conformity.java
@@ -2,21 +2,26 @@ import java.io.*;
 import java.util.*;
 public class Main
 {
-	public static void main(String args[]){
+	public static void main(String args[])
+	{
        Scanner sc=new Scanner(System.in);
-		while(sc.hasNextInt()){
+		while(sc.hasNextInt())
+		{
 			int t=sc.nextInt();
 			if(t==0)
 				break;
 			long[] abc=new long[t];
-			for(int i=0;i<t;i++){
+			for(int i=0;i<t;i++)
+			{
 				int[] a=new int[5];
-				for(int j=0;j<5;j++){
+				for(int j=0;j<5;j++)
+				{
 					a[j]=sc.nextInt();
 				}
 				Arrays.sort(a);
 				long c=0;
-				for(int j=0;j<5;j++){
+				for(int j=0;j<5;j++)
+				{
 					c=c*1000+a[j];
 				}
 				abc[i]=c;
@@ -26,14 +31,17 @@ public class Main
 			int count=1;
 			int maxcount=0;
 			int ans=0;
-			for(int i=1;i<t;i++){
+			for(int i=1;i<t;i++)
+			{
 				if(curr==abc[i])
 					count++;
-				else{
+				else
+				{
 					curr=abc[i];
 					if(maxcount==count)
 						ans+=count;
-					else if(maxcount<count){
+					else if(maxcount<count)
+					{
 						ans=count;
 						maxcount=count;
 					}
